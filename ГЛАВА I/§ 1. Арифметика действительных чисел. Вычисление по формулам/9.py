78f383a0c7f68210c44b2114f61c13d7dce8b5b8
gamau <kronos2801@gmail.com>
2023-03-03 23:23:38 +0300

    Update 9.py

--- ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/9.py
+++ ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/9.py
@@ -1,7 +1,16 @@
 def resistances(r1, r2, r3):
-    """ Функция находит сопротивление соединения"""
+    """ Функция находит сопротивление параллельного соединения"""
     R = 1 / (1 / r1 + 1 / r2 + 1 / r3)
     return f'Эквивалентное сопротивление: {R}'
 
 
 print(resistances(1, 2, 3))
+# Некорректные решения: 
+print(resistances(0, 2, 3))
+print(resistances(1, 0, 3))
+print(resistances(1, 2, 0))
+print(resistances(1, -2, 3))
+
+
+
+

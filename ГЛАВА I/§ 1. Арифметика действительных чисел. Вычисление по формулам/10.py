4f550c250da1783273a19cc8ff613f3403c5a5c0
gamau <kronos2801@gmail.com>
2023-03-03 23:28:06 +0300

    Update 10.py

--- ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/10.py
+++ ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/10.py
@@ -11,3 +11,7 @@ def time_of_the_fall_of_stone(h, g):
 
 
 print(time_of_the_fall_of_stone(h1, g=9.81))
+# Некорректные решения: 
+print(time_of_the_fall_of_stone(-2, g=9.81))
+print(time_of_the_fall_of_stone(10000, 0))
+

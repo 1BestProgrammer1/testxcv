628a6088b8b05e85c87214f02c60bc0f0739fbae
gamau <kronos2801@gmail.com>
2023-03-03 22:58:06 +0300

    Update 1..py

--- ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/1..py
+++ ГЛАВА I/§ 1. Арифметика действительных чисел. Вычисление по формулам/1..py
@@ -1,8 +1,10 @@
 number1 = int(input("Введите число: "))
 number2 = int(input("Введите число: "))
+# я, как пользователь, буду недоумевать, почему программа просит меня дважды ввести число. Я же уже ввел одно! 
 
 total_difference = number1 - number2
 total_composition = number1 + number2
 
 print(f"разность чисел: {total_difference}")
 print(f"произведение чисел: {total_composition}")
+# если я введу числа 2 и 3, на выхоже получу 5. Но разве "Произведение чисел" 2 и 3 равно 5?
